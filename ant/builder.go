0f4198dd4b87415effab044fceffade836f0f449
Hamid <7atashfeshan@gmail.com>
2017-07-19 07:04:16 +0430

    clean old codes

--- ant/builder.go
+++ ant/builder.go
@@ -2,57 +2,17 @@ package ant
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"text/template"
 )
 
-func templSerivces(services []ServiceView) {
-	tpl := template.New("go_interface")
-	tplGoInterface, err := ioutil.ReadFile(TEMPLATES_DIR + "rpc.tgo")
-	noErr(err)
-	tpl, err = tpl.Parse(string(tplGoInterface))
-	noErr(err)
-
-	s := struct {
-		Services []ServiceView
-	}{
-		Services: services,
-	}
-
-	bts := bytes.NewBufferString("")
-	err = tpl.Execute(bts, s)
-	noErr(err)
-
-	fmt.Println(bts.String())
-	ioutil.WriteFile(DIR_OUTPUT, []byte(bts.String()), os.ModeType)
-}
-
 func build(gen *GenOut) {
-    /////// Go RPC generator ////////
-   /* tplRpc:= getTemplate("rpc.tgo")
-    btsRpc := bytes.NewBufferString("")
-    err := tplRpc.Execute(btsRpc, gen)
-    noErr(err)*/
-
     OutGoRPCsStr := getTemplateFull("rpc.tgo", gen)
-
-
     writeOutput("pb__gen_ant.go", OutGoRPCsStr )
 
     /////// Enums /////////////////
 
-
-}
-
-func getTemplate(tplName string) *template.Template {
-	tpl := template.New("go_interface" + tplName)
-	tplGoInterface, err := ioutil.ReadFile(TEMPLATES_DIR + tplName)
-	noErr(err)
-	tpl, err = tpl.Parse(string(tplGoInterface))
-	noErr(err)
-	return tpl
 }
 
 func getTemplateFull(tplName string, gen *GenOut) string {
--- ant/runner.go
+++ ant/runner.go
@@ -1,45 +1,14 @@
 package ant
 
 import (
-	"fmt"
 	"github.com/dsymonds/gotoc/parser"
 	"io/ioutil"
-	"ms/sun/helper"
 )
 
-const OUTPUT_RPC = `C:\Go\_gopath\src\ms\sun\models\x\pb__gen_ant.go` //"./play/gen_sample_out.go"
 const OUTPUT_DIR = `C:\Go\_gopath\src\ms\sun\models\x\` //"./play/gen_sample_out.go"
 const TEMPLATES_DIR = "./templates/"                                  //relative to main func of parent directory
 
 func Run() {
-	const DIR_PROTOS = `C:\Go\_gopath\src\ms\sun\models\protos`
-	files, err := ioutil.ReadDir(DIR_PROTOS)
-	noErr(err)
-	protos := make([]string, len(files))
-	for i, f := range files {
-		protos[i] = f.Name()
-	}
-
-	//ast, err := parser.ParseFiles([]string{"1.proto"}, []string{"./play/"})
-	ast, err := parser.ParseFiles(protos, []string{DIR_PROTOS})
-
-	fmt.Println(ast, err)
-	fmt.Println(err)
-	for _, f := range ast.Files {
-		//helper.PertyPrint(f)
-
-		_ = f
-	}
-
-	helper.PertyPrint(ExtractAllServicesViews(ast))
-	helper.PertyPrint(ExtractAllMessagesViews(ast))
-	helper.PertyPrint(ExtractAllEnumsViews(ast))
-
-	templSerivces(ExtractAllServicesViews(ast))
-
-}
-
-func Run2() {
     const DIR_PROTOS = `C:\Go\_gopath\src\ms\sun\models\protos`
     files, err := ioutil.ReadDir(DIR_PROTOS)
     noErr(err)
