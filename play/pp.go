5f3754287ce28d66761f0831c73b1c68c339763a
Hamid <7atashfeshan@gmail.com>
2017-07-31 13:46:45 +0430

    ???

---
+++ play/pp.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/dsymonds/gotoc/parser"
+	"io/ioutil"
+	"ms/sun/helper"
+	"strings"
+)
+
+func main() {
+
+	const DIR_PROTOS = `C:\Go\_gopath\src\ms\ant\play\`
+	files, err := ioutil.ReadDir(DIR_PROTOS)
+	helper.NoErr(err)
+	protos := make([]string, 0, len(files))
+	for _, f := range files {
+		if strings.Contains(f.Name(), ".proto") {
+			protos = append(protos, f.Name())
+		}
+	}
+    _ = protos
+
+	ast, err := parser.ParseFiles([]string{"1.proto"}, []string{"./", DIR_PROTOS})
+    helper.NoErr(err)
+	helper.PertyPrint(ast.Files)
+}
